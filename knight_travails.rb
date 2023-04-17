9878576a75ee5f592bda74695e08f5eb071836ba
akea-tolentino <jamesbradley.tolentino@gmail.com>
2023-04-17 11:13:59 -0700

    defined method #valid_moves that generates an array of valid moves

--- knight_travails.rb
+++ knight_travails.rb
@@ -1,9 +1,24 @@
-require_relative "PolyTreeNode"
+require_relative "tree_node.rb"
+require 'byebug'
 
 class KnightPathFinder
 
     def self.valid_moves(pos)
-        
+        potential_move = [[-1, -2], [-1, 2], [1, -2], [1, 2]]
+        valid_positions = []
+
+        potential_move.each do |move|
+            new_row = pos[0] + move[0]
+            new_col = pos[1] + move[1]
+            #debugger
+
+            if (0..7).to_a.include?(new_row) && (0..7).to_a.include?(new_col)
+                valid_positions << [new_row, new_col]
+                valid_positions << [new_col, new_row]
+            end
+
+        end
+        valid_positions
     end
 
     attr_reader :root_node 
