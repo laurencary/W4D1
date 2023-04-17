4a545fbe2ecc6772528f592697d487dbb67c0f23
akea-tolentino <jamesbradley.tolentino@gmail.com>
2023-04-17 14:51:46 -0700

    defined children method

--- tic_tac_toe_ai/lib/tic_tac_toe_node.rb
+++ tic_tac_toe_ai/lib/tic_tac_toe_node.rb
@@ -1,7 +1,13 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+
+  attr_reader :board, :next_mover_mark, :prev_move_pos
+
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
   end
 
   def losing_node?(evaluator)
@@ -12,6 +18,14 @@ class TicTacToeNode
 
   # This method generates an array of all moves that can be made after
   # the current move.
+  
   def children
+    @board.open_positions.map do |pos|
+      state = @board.dup
+      state[pos] = next_mover_mark
+      @prev_move_pos = pos
+      TicTacToeNode.new(state, state.next_mark, @prev_move_pos)
+    end
   end
+
 end
