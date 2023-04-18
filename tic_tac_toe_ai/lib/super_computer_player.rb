e8c7ff2d4c8ea9b1fee56d4cc80348ec856cc4ef
laurencary <armstronglg720@gmail.com>
2023-04-17 17:58:49 -0700

    defined super computer player - almost finished

--- tic_tac_toe_ai/lib/super_computer_player.rb
+++ tic_tac_toe_ai/lib/super_computer_player.rb
@@ -1,7 +1,24 @@
 require_relative 'tic_tac_toe_node'
+require 'byebug'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = TicTacToeNode.new(game.board, mark)
+    debugger
+    children_arr = node.children
+    children_arr.each do |child|
+      if child.winning_node?(mark)
+        return child.prev_move_pos
+      end
+    end
+    
+    children_arr.each do |child|
+      if !child.losing_node?(mark)
+        return child.prev_move_pos
+      end
+    end  
+    
+    raise 'you gon lose'
   end
 end
 
